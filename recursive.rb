4bb30122142882e71b771827fa47f0995b136510
Michał Bielawski <michal.bielawski02@gmail.com>
2024-06-14 23:14:11 +0200

    merge sort solved

--- recursive.rb
+++ recursive.rb
@@ -80,3 +80,23 @@ def fibonacci(num)
 
   fibonacci(num - 1) + fibonacci(num - 2)
 end
+
+def merge_sort(ary)
+  return ary if ary.length == 1
+
+  left_half = merge_sort(ary[0...ary.length / 2])
+  right_half = merge_sort(ary[ary.length / 2..])
+  result = []
+  until result.length == ary.length
+    if left_half[0].nil?
+      result += right_half
+    elsif right_half[0].nil?
+      result += left_half
+    else
+      result << (left_half[0] < right_half[0] ? left_half.shift : right_half.shift)
+    end
+  end
+  result
+end
+array = [4, 657, 32, 1, 4, 7, 976_787, 2]
+merge_sort(array)
